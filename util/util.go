8a4bf3a2450e4f1060b5878d2c451663391297d6
Nick Rushton <nick@hasoffers.com>
2016-06-12 23:44:50 -0700

    Version 1.1.0 * Updated to use go1.6 * Updates to use official Amazon aws-sdk-go * performance improvements especially in parallel listing * now uses vendored dependencies through Godep * more robust options for get and stream * temporarily removed del function

--- util/util.go
+++ util/util.go
@@ -1,36 +1,82 @@
 package util
 
 import (
-	"log"
-	"strings"
+	"bufio"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"regexp"
+	"syscall"
 
-	"github.com/AdRoll/goamz/aws"
-	"github.com/AdRoll/goamz/s3"
-	"github.com/TuneOSS/fasts3/awswrapper"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-// parseS3Uri parses a s3 uri into it's bucket and prefix
-func ParseS3Uri(s3Uri string) (bucket string, prefix string) {
-	s3UriParts := strings.Split(s3Uri, "/")
-	prefix = strings.Join(s3UriParts[3:], "/")
-	bucket = s3UriParts[2]
+type s3List []string
+
+// Set overrides kingping's Set method to validate value for s3 URIs
+func (s *s3List) Set(value string) error {
+	hasMatch, err := regexp.MatchString("^s3://", value)
+	if err != nil {
+		return err
+	}
+	if !hasMatch {
+		return fmt.Errorf("%s not a valid S3 uri, Please enter a valid S3 uri. Ex: s3://mary/had/a/little/lamb\n", value)
+	} else {
+		*s = append(*s, value)
+		return nil
+	}
+}
+
+func (s *s3List) String() string {
+	return ""
+}
+
+// IsCumulative specifies S3List as a cumulative argument
+func (s *s3List) IsCumulative() bool {
+	return true
+}
+
+// S3List creates a new S3List kingpin setting
+func S3List(s kingpin.Settings) (target *[]string) {
+	target = new([]string)
+	s.SetValue((*s3List)(target))
 	return
 }
 
-// GetBucket builds a s3 connection retrieving the bucket
-func GetBucket(bucket string) *s3.Bucket {
-	auth, err := awswrapper.GetAwsAuth()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	b := s3.New(auth, aws.USEast).Bucket(bucket)
-	loc, err := b.Location()
-	if err != nil {
-		log.Fatalln(err)
+func GetNumFileDescriptors() (uint64, error) {
+	var rLimit syscall.Rlimit
 
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	return rLimit.Cur, err
+}
+
+// getReaderByExt is a factory for reader based on the extension of the key
+func GetReaderByExt(reader io.ReadCloser, key string) (*bufio.Reader, error) {
+	ext := path.Ext(key)
+	if ext == ".gz" || ext == ".gzip" {
+		gzReader, err := gzip.NewReader(reader)
+		if err != nil {
+			return bufio.NewReader(reader), nil
+		}
+		return bufio.NewReader(gzReader), nil
+	} else {
+		return bufio.NewReader(reader), nil
 	}
-	if aws.GetRegion(loc) != aws.USEast {
-		b = s3.New(auth, aws.GetRegion(loc)).Bucket(bucket)
+}
+
+// createPathIfNotExists takes a path and creates
+// it if it doesn't exist
+func CreatePathIfNotExists(path string) error {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		err := os.MkdirAll(path, 0755)
+		if err != nil {
+			return err
+		}
+	} else {
+		return nil
 	}
-	return b
+	return nil
 }
