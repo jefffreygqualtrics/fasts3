b009d89da743cfcd0cb26870fb85abc266529f07
Nick Rushton <nick@hasoffers.com>
2016-07-15 13:05:54 -0700

    Fix hard coded delmiter logic when searching depths

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func Ls(svc *s3.S3, s3Uris []string, recursive bool, delimiter string, searchDep
 			newS3Uris := make([]string, 0)
 			for itm := range wrap.ListAll(s3Uris, false, delimiter, keyRegex) {
 				if itm.IsPrefix {
-					newS3Uris = append(newS3Uris, strings.TrimRight(*itm.FullKey, "/")+"/")
+					newS3Uris = append(newS3Uris, strings.TrimRight(*itm.FullKey, delimiter)+delimiter)
 				} else {
 					outChan <- itm
 				}
