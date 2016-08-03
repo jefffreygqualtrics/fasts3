476ca16ed9f7b77fe610c6923e2e3fe32de0cc94
Nick Rushton <nick@hasoffers.com>
2016-08-03 15:45:10 -0700

    Make stream function non-blocking

--- s3wrapper/s3.go
+++ s3wrapper/s3.go
@@ -157,46 +157,47 @@ func (w *S3Wrapper) GetReader(bucket string, key string) (io.ReadCloser, error) 
 func (w *S3Wrapper) Stream(keys chan *ListOutput, includeKeyName bool) chan string {
 	lines := make(chan string, 10000)
 	var wg sync.WaitGroup
-	for key := range keys {
-		wg.Add(1)
-		go func(key *ListOutput) {
-			defer wg.Done()
-			w.concurrencySemaphore <- struct{}{}
-			defer func() { <-w.concurrencySemaphore }()
+	go func() {
+		for key := range keys {
+			wg.Add(1)
+			go func(key *ListOutput) {
+				defer wg.Done()
+				w.concurrencySemaphore <- struct{}{}
+				defer func() { <-w.concurrencySemaphore }()
 
-			reader, err := w.GetReader(*key.Bucket, *key.Key)
-			if err != nil {
-				panic(err)
-			}
-			ext_reader, err := util.GetReaderByExt(reader, *key.Key)
-			if err != nil {
-				panic(err)
-			}
-
-			for {
-				line, _, err := ext_reader.ReadLine()
+				reader, err := w.GetReader(*key.Bucket, *key.Key)
 				if err != nil {
-					if err.Error() == "EOF" {
-						break
+					panic(err)
+				}
+				ext_reader, err := util.GetReaderByExt(reader, *key.Key)
+				if err != nil {
+					panic(err)
+				}
+
+				for {
+					line, _, err := ext_reader.ReadLine()
+					if err != nil {
+						if err.Error() == "EOF" {
+							break
+						} else {
+							log.Fatalln(err)
+						}
+					}
+					if includeKeyName {
+						lines <- fmt.Sprintf("[%s] %s", *key.FullKey, string(line))
 					} else {
-						log.Fatalln(err)
+						lines <- fmt.Sprintf("%s", string(line))
 					}
 				}
-				if includeKeyName {
-					lines <- fmt.Sprintf("[%s] %s", *key.FullKey, string(line))
-				} else {
-					lines <- fmt.Sprintf("%s", string(line))
-				}
-			}
-		}(key)
-	}
-	go func() {
-		wg.Wait()
-		close(lines)
+			}(key)
+		}
+		go func() {
+			wg.Wait()
+			close(lines)
+		}()
 	}()
 
 	return lines
-
 }
 
 func (w *S3Wrapper) GetAll(keys chan *ListOutput) chan *ListOutput {
@@ -215,6 +216,7 @@ func (w *S3Wrapper) GetAll(keys chan *ListOutput) chan *ListOutput {
 				dir := strings.Join(parts[0:len(parts)-1], "/")
 				util.CreatePathIfNotExists(dir)
 				reader, err := w.GetReader(*k.Bucket, *k.Key)
+				defer reader.Close()
 				if err != nil {
 					panic(err)
 				}
